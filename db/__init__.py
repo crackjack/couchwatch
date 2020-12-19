7de3e19c387b579094c728ab7c47617d61ca713e
Nitesh Rijal <rijal.it@gmail.com>
2020-12-19 13:37:07 -0500

    add sqlalchemy and initial Base db

---
+++ db/__init__.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+SQLALCHEMY_SQLITE_URL = "sqlite:///./couchwatch.db"
+
+sqlite_engine = create_engine(SQLALCHEMY_SQLITE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=sqlite_engine)
+
+Base = declarative_base()
