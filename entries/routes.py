0c2a5622492f6e6ba7fbd86adac3bb50bfb7d84e
Nitesh Rijal <rijal.it@gmail.com>
2020-12-19 21:10:09 -0500

    update route tests

--- entries/routes.py
+++ entries/routes.py
@@ -1,38 +1,44 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Response, status
 from typing import List
 from sqlalchemy.orm import Session
 
-from db import SessionLocal
-from db import entries as db_entries
-from .schemas import Entry, CreateEntry
-
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+from db import get_db, entries as db_entries
+from .schemas import Entry
 
 
 router = APIRouter()
 
 
-@router.get("/browse/{entry_id}", status_code=status.HTTP_200_OK, response_model=Entry)
-def get_entry(entry_id: int, db: Session = Depends(get_db)):
-    entry = db_entries.get_single_entry(db, entry_id=entry_id)
+@router.get("/browse/{entry_id}", response_model=Entry, responses={
+    404: {"detail": "Entry not found."}
+})
+def get_entry(netflix_id: str, db: Session = Depends(get_db)):
+    entry = db_entries.get_single_entry(db, netflix_id=netflix_id)
     if not entry:
-        raise HTTPException(status_code=404, detail="Entry not Found")
-    return {"entry": entry}
+        raise HTTPException(status_code=404, detail="Entry not found.")
+    return entry
 
 
-@router.post("/browse/", status_code=status.HTTP_201_CREATED, response_model=Entry)
-def create_entry(entry: CreateEntry, db: Session = Depends(get_db)):
-    entries = db_entries.create_entry(db, entry)
-    return entries
+@router.post("/browse/", status_code=status.HTTP_201_CREATED, response_model=Entry, responses={
+    400: {"detail": "Something bad happened."}
+})
+def create_entry(entry: Entry, db: Session = Depends(get_db)):
+    entry = db_entries.create_entry(db, entry)
+    if not entry:
+        raise HTTPException(status_code=400, detail="Something bad happened.")
+    return entry
 
 
-@router.get("/browse/", status_code=status.HTTP_200_OK, response_model=List[Entry])
+@router.get("/browse/", response_model=List[Entry])
 def get_entries(db: Session = Depends(get_db)):
     entries = db_entries.get_entries(db)
     return entries
+
+
+@router.delete("/browse/{entry_id}", response_class=Response, responses={
+    200: {"message": "Successfully Deleted"},
+    404: {"message": "Entry not found."}
+})
+def delete_entry(netflix_id: str, db: Session = Depends(get_db)):
+    return Response(status_code=200) if db_entries.delete_entry(db, netflix_id=netflix_id) else Response(status_code=404)
+
--- tests/entries/test_routes.py
+++ tests/entries/test_routes.py
@@ -1,35 +1,58 @@
+import os
 from fastapi.testclient import TestClient
 
 from main import app
-from tests import override_get_db, test_sqlite_engine, TestingSessionLocal
+from tests import override_get_db, test_sqlite_engine
 
 from entries.routes import get_db
 from entries.models import Base
 
 
 app.dependency_overrides[get_db] = override_get_db
-client = TestClient(app)
 
-Base.metadata.create_all(bind=test_sqlite_engine)
+client = TestClient(app)
 
 
 def test_get_entries():
+    Base.metadata.create_all(bind=test_sqlite_engine)
     response = client.get("/browse/")
     assert response.status_code == 200
     assert response.json() == []
+    Base.metadata.drop_all(bind=test_sqlite_engine)
 
 
 def test_get_entry():
-    response = client.get("/browse/12345")
+    Base.metadata.create_all(bind=test_sqlite_engine)
+    response = client.get("/browse/82739")
     assert response.status_code == 404
-    assert response.json() == {"detail": "Entry not Found"}
-    Base.metadata.create_all(test_sqlite_engine)
+
+    post_data = {"netflix_id": "1234", "name": "fake"}
+    client.post("/browse/", json=post_data)
+    response = client.get("/browse/1234")
+    assert response.status_code == 200
+
+    Base.metadata.drop_all(bind=test_sqlite_engine)
 
 
 def test_create_entry():
-    post_data = {"netflix_id": 1234, "name": "fake"}
+    Base.metadata.create_all(bind=test_sqlite_engine)
+    post_data = {"netflix_id": "1234", "name": "fake"}
     response = client.post("/browse/", json=post_data)
     assert response.status_code == 201
     assert response.json()["name"] == "fake"
-    assert "netflix_id" not in response.json().keys()
-    Base.metadata.create_all(test_sqlite_engine)
+    assert response.json()["netflix_id"] == "1234"
+    Base.metadata.drop_all(bind=test_sqlite_engine)
+
+
+def test_delete_entry():
+    Base.metadata.create_all(bind=test_sqlite_engine)
+    post_data = {"netflix_id": "1234", "name": "fake"}
+    client.post("/browse/", json=post_data)
+
+    response = client.delete("/browse/4321")
+    assert response.status_code == 404
+
+    response = client.delete("/browse/1234")
+    assert response.status_code == 200
+
+    Base.metadata.drop_all(bind=test_sqlite_engine)
