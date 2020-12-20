999ed361d16764a9e09b3f965df9338102341fcf
Nitesh Rijal <rijal.it@gmail.com>
2020-12-19 19:08:05 -0500

    added routes unittest

--- entries/models.py
+++ entries/models.py
@@ -5,6 +5,6 @@ from sqlalchemy import Column, Integer, String
 class Entries(Base):
     __tablename__ = "entries"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     netflix_id = Column(String)
     name = Column(String, nullable=False)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+
 from fastapi import FastAPI
 from db import sqlite_engine
-
 from entries import routes as entry_routes
 from entries import models as entry_models
 
@@ -16,3 +16,8 @@ entry_models.Base.metadata.create_all(bind=sqlite_engine)
 @app.get("/")
 def ping():
     return {"status": "It Works!"}
+
+
+# import uvicorn
+# if __name__ == "__main__":
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
