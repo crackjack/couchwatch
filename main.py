b65cda33700c36abe3d00391ac2c7b872f371ada
Nitesh Rijal <rijal.it@gmail.com>
2020-12-21 16:51:25 -0500

    added jinja2 and datatables showing up

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-
 from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
 from db import sqlite_engine
 from entries import routes as entry_routes
 from entries import schemas as entry_schemas
@@ -15,9 +15,81 @@ app.include_router(entry_routes.router)
 entry_schemas.Base.metadata.create_all(bind=sqlite_engine)
 
 
-@app.get("/")
-def ping():
-    return {"status": "It Works!"}
+@app.get(
+    "/",
+    response_class=HTMLResponse
+)
+def home():
+    table_rows = """
+        <tr>
+            <td>Tiger Nixon</td>
+            <td>System Architect</td>
+            <td>Edinburgh</td>
+            <td>61</td>
+            <td>2011/04/25</td>
+            <td>$320,800</td>
+        </tr>
+    """
+    return f"""
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <title>Welcome to Couch Watch</title>
+            <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+            <link rel="stylesheet" href="https://cdn.datatables.net/1.10.22/css/dataTables.bulma.min.css">
+        </head>
+        <body>
+            <section class="hero">
+              <div class="hero-body">
+                <div class="container">
+                  <h1 class="title">
+                    Couch Watch
+                  </h1>
+                  <h2 class="subtitle">
+                    Netflix Movies and TV Shows
+                  </h2>
+                </div>
+              </div>
+            </section>
+            <table id="shows" class="table is-striped is-fullwidth">
+        <thead>
+            <tr>
+                <th>Show ID</th>
+                <th>Title</th>
+                <th>Type</th>
+                <th>Duration</th>
+                <th>Added date</th>
+                <th>Year</th>
+            </tr>
+        </thead>
+        <tbody>
+            {table_rows}
+        </tbody>
+        <tfoot>
+            <tr>
+                <th>Show ID</th>
+                <th>Title</th>
+                <th>Type</th>
+                <th>Duration</th>
+                <th>Added date</th>
+                <th>Year</th>
+            </tr>
+        </tfoot>
+    </table>
+            <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+            <script defer src="https://use.fontawesome.com/releases/v5.14.0/js/all.js"></script>
+            <script defer src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+            <script defer src="https://cdn.datatables.net/1.10.22/js/dataTables.bulma.min.js"></script>
+            <script type="text/javascript">
+                $(document).ready(function() {{
+                    $('#shows').DataTable();
+                }});
+            </script>
+        </body>
+    </html>
+    """
 
 
 @app.post("/upload/")
